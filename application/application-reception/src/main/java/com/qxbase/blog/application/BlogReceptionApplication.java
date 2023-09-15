b353cee110355c61c6a7995e3d8ce016ae2713f1
青旬 <947338658@qq.com>
2023-09-16 00:03:25 +0800

    完成   - swagger 读取接口问题

--- application/application-reception/src/main/java/com/qxbase/blog/application/BlogReceptionApplication.java
+++ application/application-reception/src/main/java/com/qxbase/blog/application/BlogReceptionApplication.java
@@ -3,9 +3,15 @@ package com.qxbase.blog.application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.qxbase.blog.**"})
 @MapperScan({"com.qxbase.blog.**.mapper"})
+@EnableConfigurationProperties
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableScheduling
 public class BlogReceptionApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogReceptionApplication.class, args);
--- config/swagger-config/src/main/java/com/qxbase/blog/config/SwaggerConfig.java
+++ config/swagger-config/src/main/java/com/qxbase/blog/config/SwaggerConfig.java
@@ -6,15 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.annotation.Resource;
-import java.util.ArrayList;
 
 /**
  * @author ALsW
@@ -28,12 +23,12 @@ public class SwaggerConfig {
 
 	 @Bean
 	 public Docket createRestApi() {
-	 	return new Docket(DocumentationType.OAS_30)
-	 			.apiInfo(apiInfo()) // 用于生成API信息
-	 			.select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
-	 			.apis(RequestHandlerSelectors.withClassAnnotation(Api.class)) // 用于指定扫描哪个包下的接口
-	 			.paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
-	 			.build();
+		 return new Docket(DocumentationType.SWAGGER_2)
+				 .apiInfo(apiInfo())
+				 .select()
+				 .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+				 .paths(PathSelectors.any())
+				 .build();
 	 }
 
 	@Bean
